76293da911fa6e619b15eb8cd7fa52514ef1af2d
Mark Loyzer <mark.loyzer@gmail.com>
2013-06-16 12:47:04 -0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,14 +41,11 @@ describe "User pages" do
         it { should have_content('Password can\'t be blank') }       
         it { should have_content('Password is too short (minimum is 6 characters)') }       
         it { should have_content('Password confirmation can\'t be blank') }       
-      
-
-
       end 
     end
 
     describe "with valid information" do
-      before { valid_signin(user) }
+      before { valid_signup }
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -59,8 +56,87 @@ describe "User pages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_welcome_message('Welcome') }
         it { should have_link('Sign out') }
+      end
+    end
+  end
+
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_selector('h1', text: "Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+
+    describe "with valid information" do
+      let(:new_name) { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before { valid_edit(user, new_name, new_email) }
+
+      it { should have_title(new_name) }
+      it { should have_alert_success }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should == new_name }
+      specify { user.reload.email.should == new_email }
+
+    end
+  end
+
+
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_selector('h1', text: 'All users') }
+
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all) { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
+
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect { click_link('delete') }.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -8,14 +10,60 @@ def full_title(page_title)
 end
 
 
+def valid_signup
+  fill_in "Name",     with: "Example User"
+  fill_in "Email",    with: "user@example.com"
+  fill_in "Password", with: "foobar"
+  fill_in "Confirmation", with: "foobar"
+end
+
+
 def valid_signin(user)
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
 end
 
+def valid_edit(user, new_name, new_email)
+  fill_in "Name",             with: new_name
+  fill_in "Email",            with: new_email
+  fill_in "Password",         with: user.password
+  fill_in "Confirm Password", with: user.password
+  click_button "Save changes"
+end
+
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  
+  #Sign in when not using capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+Rspec::Matchers.define :have_welcome_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
+  end
+end
+
+
+Rspec::Matchers.define :have_title do |message|
+  match do |page|
+    page.should have_selector('title', text: message)
+  end
+end
+
+Rspec::Matchers.define :have_alert_success do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success') 
   end
 end
